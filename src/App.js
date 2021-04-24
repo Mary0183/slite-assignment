18a8dd73cef03dbeb32c981ca9136365c0cb9770
Mary <marysallent@gmail.com>
2021-04-24 20:12:33 +0200

    adding css grids

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import react from "react";
+import Logo1 from "./images/Slite.svg";
+import Logo2 from "./images/logo--color.svg";
+
+// import wave from "./images/wave.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Slite test</h1>
-      </header>
+      <div className="logo-grid">
+        <img className="logo-1" src={Logo1} />
+        <img className="logo-2" src={Logo2} />
+      </div>
+
+      <div className="container">
+        <div className="grid-container-colm">
+          <h1>columns</h1>
+        </div>
+        <div className="grid-container-row">
+          <h1>row</h1>
+        </div>
+      </div>
     </div>
   );
 }
