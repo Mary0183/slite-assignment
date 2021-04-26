6958026067c9bb834c42a5e7576678bf4527c103
Mary <marysallent@gmail.com>
2021-04-26 22:16:09 +0200

    working on Favorite expand/collapse

--- src/Components/Breadcrumbs.js
+++ src/Components/Breadcrumbs.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Breadcrumbs.css";
-import YellowStar from "../images/stroke.svg";
+import Star from "../images/stroke.svg";
 import Right from "../images/right.svg";
 
 function Breadcrumbs() {
   return (
     <div>
       <div className="topbar">
-        <img src={YellowStar} />
+        {/* <img src={YellowStar} />
         <a href="#Favorites">Favorites</a>
 
         <img src={Right} />
-        <a href="#People and culture">People and culture</a>
-
-        <img src={YellowStar} />
+        <a href="#People and culture">People and culture</a> */}
+        <div className="add-favorite">
+          <img className="bookmark-star" src={Star} />
+        </div>
       </div>
     </div>
   );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,18 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const dropdownRef = useRef(null);
+  const [favActive, setFavActive] = useState(false);
+
+  const onClick = () => setFavActive(!favActive);
+
   return (
     <div>
       <div className="grid-container-col">
         <div className="sidenav-grid">
+          <div className="menu-container favorites-container">
+            <button onClick={onClick} className="favorites">
+              <span>Favorites</span>
+            </button>
+            <nav
+              ref={dropdownRef}
+              className={`menu ${favActive ? "active" : "inactive"}`}
+            >
+              <ul>
+                <li>
+                  <Link className="people" to="/people-and-culture">
+                    People and culture
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
           <ul className="sidebar-nav">
-            <li>
-              <a className="favorites" href="#Favorites">
+            {/* <li className="favorites-container"> */}
+            {/* <a className="favorites" href="#Favorites">
                 Favorites
-              </a>
-            </li>
+              </a> */}
+            {/* </li> */}
 
             <li>
               <Link className="people" to="/people-and-culture">
